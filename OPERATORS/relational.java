9eec06ce4c557eb9abe2af5aa8cf9d147ebab9c0
THOLETI RATNA MANI <104676857+RatnamaniTholeti@users.noreply.github.com>
2023-02-22 12:28:14 +0530

    Create relational.java

---
+++ OPERATORS/relational.java
@@ -0,0 +1,24 @@
+package Pakage;
+
+import java.util.Scanner;
+
+public class Hiii {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+			int a = 10 ;
+			int b = 10;
+	
+			System.out.println(a>b);		
+			System.out.println(a<b);	
+			System.out.println(a=b);	
+			System.out.println(a>=b);	
+			System.out.println(a<=b);	
+			System.out.println(a!=b);
+			System.out.println(a==b);	
+					
+		
+	}
+}
