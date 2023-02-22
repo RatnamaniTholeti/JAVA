f54d34b3226257a071fd7ad67eabbbe1526cc4f5
THOLETI RATNA MANI <104676857+RatnamaniTholeti@users.noreply.github.com>
2023-02-22 12:21:44 +0530

    Create increment decrement.java

---
+++ OPERATORS/increment decrement.java
@@ -0,0 +1,22 @@
+package Pakage;
+
+import java.util.Scanner;
+
+public class Hiii {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		System.out.println(a++);
+    // a++ = 10 a = 11 // 
+		System.out.println(++a);
+    // ++a = 12 a = 13 //   
+		System.out.println(a--);
+    
+		System.out.println(--a);
+	}
+
+}
+
+
+
