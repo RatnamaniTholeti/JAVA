03429c5a4ea700ed331ec28544669e557caeff8a
THOLETI RATNA MANI <104676857+RatnamaniTholeti@users.noreply.github.com>
2023-02-22 12:18:25 +0530

    Create arthimetic.java

---
+++ OPERATORS/arthimetic.java
@@ -0,0 +1,36 @@
+package Pakage;
+
+import java.util.Scanner;
+
+public class Hiii {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+			int a = 10 ;
+			int b = 10;
+			boolean c = true;
+			boolean d = false;
+			
+      System.out.println(~a);
+			System.out.println(~b);
+			System.out.println(!c);
+			System.out.println(!d);
+      int s = -9;
+			System.out.println(~s);
+      
+			//ARTHIMETIC OPERATORS//
+      System.out.println(a+b);
+			System.out.println(a-b);
+			System.out.println(a*b);
+			System.out.println(a/b);
+			System.out.println(a%b);
+			
+			System.out.println(10*10/5+3-1*4/2);
+			
+		
+			
+			
+	}
+}
